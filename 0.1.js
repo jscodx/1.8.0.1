824c46fae2dd05b6141e6522008d8ab2050c6bf7
jscodx <mkhawi700@gmail.com>
2025-06-16 19:55:54 +0300

    Add files via upload

---
+++ 0.1.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function() {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://q1d4a19g-24ki1aqm-k4hk7m-ki2xm0-19xt1.blogspot.com/p/frame-01.html";
+  iframe.width = "100%";
+  iframe.height = "280px";
+  iframe.style.border = "none";
+  iframe.style.display = "block";
+  document.body.appendChild(iframe);
+});
